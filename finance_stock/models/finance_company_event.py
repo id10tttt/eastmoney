4fef5657b02a2d3e45e9ecc99ff5893794b38de5
1di0t <daiguangquan@sf-pharma.com>
2023-03-15 11:09:13 +0800

    优化和调整

--- finance_stock/models/finance_company_event.py
+++ finance_stock/models/finance_company_event.py
@@ -7,6 +7,7 @@ class FinanceCompanyEvent(models.Model):
     _description = '公司事件'
 
     stock_id = fields.Many2one('finance.stock.basic', index=True)
+    security_code = fields.Char(related='stock_id.symbol', store=True)
     name = fields.Char('事件')
     event_date = fields.Date('事件日期')
     event_type = fields.Char('类型')
